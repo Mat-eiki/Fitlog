2fd3e39108760f5b24c60b8edb745636dfaa3344
Matheus Eiki <mekreiki@gmail.com>
2025-06-12 19:53:25 -0300

    bottle add

---
+++ app.py
@@ -0,0 +1,13 @@
+from bottle import route, run, template
+from bottle import static_file
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='static')
+
+
+@route('/')
+def home():
+    return template('views/home.html', nome="Visitante")
+
+run(host='localhost', port=8080, debug=True)
